5ac27af56aa210d3a67aaf2ee79df85902acf069
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-09-21 22:46:06 +0200

    Create default app setup

--- App.tsx
+++ App.tsx
@@ -1,118 +1,199 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {configureStore, createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {persistStore, persistReducer} from 'redux-persist';
+import {PersistGate} from 'redux-persist/integration/react';
+import thunk from 'redux-thunk';
+import {useSelector, useDispatch, Provider} from 'react-redux';
+import {Text, Button, View, SafeAreaView, StyleSheet} from 'react-native';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+type CounterState = {
+  value: number;
+  status: string;
+};
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+type RootState = {
+  counter: CounterState;
+};
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+function log(...args: unknown[]) {
+  console.log(`[${new Date().toISOString().substring(11)}]`, ...args);
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+async function delay(ms?: number) {
+  log(`[DELAY] For ${ms || 0}ms`);
+  await new Promise(r => setTimeout(r, ms));
+}
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+log('------------------------------------------------');
+log('[START] Loading App.tsx file');
+
+const initialState: CounterState = {
+  value: 0,
+  status: 'idle',
+};
+
+const incrementAsync = createAsyncThunk(
+  'counter/fetchCount',
+  async (amount: number) => {
+    await delay(1500);
+    return amount;
+  },
+);
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    reset: state => ({...state, value: 0}),
+    increment: state => ({...state, value: state.value + 1}),
+    decrement: state => ({...state, value: state.value - 1}),
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(incrementAsync.pending, state => {
+        state.status = 'loading';
+      })
+      .addCase(incrementAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.value += action.payload;
+      });
+  },
+});
+
+const {increment, decrement, reset} = counterSlice.actions;
+const selectCount = (state: RootState) => state.counter.value;
+const selectStatus = (state: RootState) => state.counter.status;
+
+const decoratedStorage = {
+  async getItem(key: string) {
+    log(`[^ - GET] for key '${key}'`);
+    const raw = await EncryptedStorage.getItem(key);
+    return raw;
+  },
+  async setItem(key: string, value: string) {
+    log(`[v - SET] for key '${key}'. Value = `, value);
+    await EncryptedStorage.setItem(key, value);
+  },
+  async removeItem(key: string) {
+    log(`[x - DEL] for key '${key}`);
+    await EncryptedStorage.removeItem(key);
+  },
+};
+
+const persistConfig = {
+  key: 'root',
+  timeout: 5000,
+  storage: decoratedStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, counterSlice.reducer);
+
+const store = configureStore({
+  reducer: {
+    counter: persistedReducer,
+  },
+  middleware: [thunk],
+});
+
+const persistor = persistStore(store);
+
+const Counter = () => {
+  log('[RENDER - Counter]');
+
+  const count = useSelector(selectCount);
+  const status = useSelector(selectStatus);
+  const dispatch = useDispatch<any>(); // TODO: Get rid of the 'any' type
+  const disabled = status === 'loading';
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <>
+      <Text style={styles.number}>{count}</Text>
+      <Text
+        style={[
+          styles.paragraph,
+          status === 'loading' ? styles.active : styles.inactive,
+        ]}>
+        {status}
+      </Text>
+      <View style={styles.buttonsContainer}>
+        <Button
+          title="Decrement (-1)"
+          onPress={() => dispatch(decrement())}
+          color="firebrick"
+        />
+        <Button
+          title="Increment (+1)"
+          onPress={() => dispatch(increment())}
+          color="seagreen"
+        />
+      </View>
+      <View style={styles.buttonsContainer}>
+        <Button
+          disabled={disabled}
+          title="Delayed increment (+10)"
+          onPress={() => dispatch(incrementAsync(10))}
+        />
+      </View>
+      <View style={styles.buttonsContainer}>
+        <Button
+          disabled={disabled}
+          title="Reset!"
+          onPress={() => dispatch(reset())}
+          color="gray"
+        />
+      </View>
+    </>
+  );
+};
+
+export default function App(): JSX.Element {
+  log('[RENDER - App]');
+  const loader = (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>Loading...</Text>
+    </View>
+  );
+  return (
+    <Provider store={store}>
+      <PersistGate loading={loader} persistor={persistor}>
+        <SafeAreaView style={styles.container}>
+          <Text style={styles.paragraph}>Counter example</Text>
+          <Counter />
+        </SafeAreaView>
+      </PersistGate>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  buttonsContainer: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    gap: 8,
+    margin: 8,
+    marginTop: 16,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  paragraph: {
+    fontSize: 26,
+    textAlign: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  number: {
+    margin: 8,
+    fontSize: 48,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  active: {
+    color: 'dodgerblue',
+  },
+  inactive: {
+    color: 'silver',
   },
 });
-
-export default App;
